e64a620c296c162adf81359b27d13d846c88750e
GioByte10 <gvanni.bernal10@gmail.com>
2023-06-05 03:27:32 -0700

    PhySim

---
+++ Bouncy-Ball/sketch.js
@@ -0,0 +1,54 @@
+let ball;
+let gravity = 0.1;
+let vy = 0;
+let vx = 10;
+let cry = 1;
+let crx = 1;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    ball = new Ball(width / 2, height / 2, 20);
+}
+
+function draw() {
+    background(220);
+    vy += gravity;
+    ball.y += vy;
+
+    ball.x += vx;
+
+    if(ball.x + ball.radius >= width){
+        vx = -vx * crx;
+        ball.x = width - ball.radius;
+        ball.x += vx;
+    }
+
+    if(ball.x - ball.radius <= 0){
+        vx = -vx * crx;
+        ball.x = ball.radius;
+        ball.x += vx;
+    }
+
+
+    if (ball.y + ball.radius >= height) {
+        vy = -vy * cry;
+        ball.y = height - ball.radius;
+        ball.y += vy;
+    }
+    //console.log(vy)
+
+    // Display the ball
+    ball.display();
+}
+
+class Ball {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    display() {
+        ellipse(this.x, this.y, this.radius * 2);
+    }
+}
---
+++ Rotating-Disk/sketch.js
@@ -0,0 +1,35 @@
+let n = -0.0000000012;
+let s = 0.00000115;
+let j = -0.00047;
+let a = 0.1;
+let w = -10;
+let angle = 0;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);
+    frameRate(60);
+    noFill();
+}
+
+function draw() {
+    background(220);
+
+    translate(width/2, height/2);
+    rotate(-angle);
+    circle(0, 0, width / 3 - 60);
+    arc(0, 0, 20, 20, 90, 270)
+
+    strokeWeight(3);
+    point(0, 0);
+
+    strokeWeight(1);
+    line(0, 10, (width / 3 - 60) / 2, 10);
+    line(0, -10, (width / 3 - 60) / 2, -10);
+
+    s += n;
+    j += s;
+    a += j;
+    w += a;
+    angle += w;
+}
