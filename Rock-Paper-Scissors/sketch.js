0deae67fc289c56be9d098d969082db07887e883
GioByte10 <gvanni.bernal10@gmail.com>
2023-08-01 00:17:05 -0700

    SimLab

--- Rock-Paper-Scissors/sketch.js
+++ Rock-Paper-Scissors/sketch.js
@@ -1,5 +1,5 @@
-let n = 30;
-let r = 5;
+let n = 100;
+let radius = 5;
 
 let rockObjects = [];
 let paperObjects = [];
@@ -7,11 +7,25 @@ let scissorObjects = [];
 
 let fps = 60;
 
+let rockSound;
+let paperSound;
+let scissorSound;
+let over = false;
+let wait;
+
+function preload(){
+    rockSound = loadSound('sounds/rock.mp3');
+    paperSound = loadSound('sounds/paper.mp3');
+    scissorSound = loadSound('sounds/scissors.mp3');
+}
+
+
 function setup(){
 
     createCanvas(windowWidth, windowHeight);
     background(220);
     textSize(20);
+    textAlign(CENTER);
     frameRate(fps);
 
     if(navigator.userAgent.match(/iPhone|iPod|Android|BlackBerry|Windows Phone/i))
@@ -27,15 +41,30 @@ function setup(){
         else
             scissorObjects.push(new RPSObject(random(width), random(height), '✂️'));
 
-    // rockObjects.push(new object(width / 2, height / 2, '🪨'));
-    // paperObjects.push(new object(random(width), random(height), '📜'));
-    // paperObjects.push(new object(random(width), random(height), '📜'));
-
 }
 
 function draw(){
 
-    background(200);
+    if(over){
+        if(frameCount % (fps * 5) < (3 * fps)){
+            if (!wait) {
+                r = random(255);
+                g = random(255);
+                b = random(255);
+                wait = true;
+            }
+            background(r, g, b);
+
+        }else{
+            background(200);
+            wait = false;
+        }
+
+    }else {
+        background(200);
+    }
+
+    textSize(20);
 
     for(let i = 0; i < rockObjects.length; i++)
         rockObjects[i].update();
@@ -45,6 +74,21 @@ function draw(){
 
     for(let i = 0; i < scissorObjects.length; i++)
         scissorObjects[i].update();
+
+    textSize(50);
+
+    if(rockObjects.length === n) {
+        text('🪨 wins!', width / 2, height / 2);
+        over = true;
+
+    }else if(paperObjects.length === n) {
+        text('📜 wins!', width / 2, height / 2);
+        over = true;
+
+    }else if(scissorObjects.length === n) {
+        text('✂️ wins!', width / 2, height / 2);
+        over = true;
+    }
 
 }
 
@@ -57,20 +101,20 @@ class RPSObject {
     update(){
 
         if(this.emoji === '🪨')
-            this.findTarget(rockObjects, scissorObjects);
+            this.findTarget(rockObjects, scissorObjects, rockSound);
 
         else if(this.emoji === '📜')
-            this.findTarget(paperObjects, rockObjects);
+            this.findTarget(paperObjects, rockObjects, paperSound);
 
         else if(this.emoji === '✂️')
-            this.findTarget(scissorObjects, paperObjects);
+            this.findTarget(scissorObjects, paperObjects, scissorSound);
 
         this.checkBoundaries();
 
         text(this.emoji, this.position.x, this.position.y);
     }
 
-    findTarget(currentArray, targetArray){
+    findTarget(currentArray, targetArray, sound){
         let closestTarget = createVector(Infinity, Infinity);
         let other;
 
@@ -83,29 +127,30 @@ class RPSObject {
         this.position.add(createVector(random(-2, 2), random(-2, 2)))
 
         if(closestTarget.mag() !== Infinity) {
-            if (closestTarget.mag() < 2 * r) {
+            if (closestTarget.mag() < 2 * radius) {
                 other.emoji = this.emoji;
+                sound.play();
                 targetArray.splice(targetArray.indexOf(other), 1);
                 currentArray.push(other);
 
             } else {
-                this.position.add(closestTarget.normalize().mult(0.5));
+                this.position.add(closestTarget.normalize().mult(0.7));
             }
         }
 
     }
 
     checkBoundaries(){
-        if(this.position.x < r)
-            this.position.x = r;
+        if(this.position.x < radius)
+            this.position.x = radius;
 
-        else if(this.position.x > width - r)
-            this.position.x = width;
+        else if(this.position.x > width - radius)
+            this.position.x = width - radius;
 
-        if(this.position.y < r)
-            this.position.y = r;
+        if(this.position.y < radius)
+            this.position.y = radius;
 
-        else if(this.position.y > height - r)
-            this.position.y = height - r;
+        else if(this.position.y > height - radius)
+            this.position.y = height - radius;
     }
 }
