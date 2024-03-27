3fc507ef0264cb73b94f4e6a4b90d9e90593273e
GioByte10 <gvanni.bernal10@gmail.com>
2024-03-26 22:22:43 -0700

    Added Knoop

---
+++ _lib/addP5.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/p5@1.9.2/lib/p5.js';
+
+    document.head.appendChild(script);
+});
