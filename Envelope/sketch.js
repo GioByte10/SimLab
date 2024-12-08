c7d97a968d95ae997d16de61940ac528ed6c64ee
Giovanni Bernal <gvanni.bernal10@gmail.com>
2024-12-08 02:27:14 -0800

    Added envelope

--- Envelope/sketch.js
+++ Envelope/sketch.js
@@ -1,354 +1,125 @@
 const FPS = 60;
-const duration = 10;
-let points = new Float32Array(0);
+let env;
+
+let duration = 5;
 let timeStamp = -(duration * 1000 + 1);
 let t;
-let pausedT;
-let dark = true;
-let paused = false;
+let axesLength = 400
 
-let checkRandomColor;
-let checkBackgroundColor;
-let checkShowBezierCurve;
-let checkShowConstantSpeed;
+let x0 = 150;
+let y0 = 150;
 
-let curvePoints = [];
-let curvePointsSpeed = [];
-let currentCurve = -1;
+let x1 = x0;
+let y1 = y0 + axesLength;
 
-let env;
+let x2 = x0 + axesLength;
+let y2 = y0;
+
 let sliderNumberLines;
 
-let rr;
-let rg;
-let rb;
-
-const x = 0;
-const y = 1;
-
-const instructions = [
-    "Bézier Curve Simulation",
-    "Click on the screen to place points",
-    "Press SPACE to start simulation",
-    "Press C to clear the screen",
-    "Press P to pause simulation",
-]
-
-p5.disableFriendlyErrors = true;
-
-function setup() {
+function setup(){
     createCanvas(windowWidth, windowHeight);
     frameRate(FPS);
-    colorMode(HSB, 360, 100, 100);
 
     if(navigator.userAgent.match(/iPhone|iPad|iPod|Android|webOs|BlackBerry|Windows Phone/i))
         pixelDensity(1);
 
+    env = new Envelope;
     createUI();
-
-    env = new Envelope(windowWidth / 2, windowHeight / 2);
-
-}
-
-function staticSetup(){
-    push();
-
-
-    dark ? (noStroke(), fill(255)) : (noStroke(), fill(0))
-
-    textSize(16);
-    textAlign(LEFT);
-
-    for(let i = 0; i < instructions.length; i++)
-        text(instructions[i], 15, 8 + 22 * (i + 1));
-
-    textSize(16);
-    textAlign(RIGHT);
-
-    if(t < duration)
-        text(t.toPrecision(3), windowWidth - 20, windowHeight - 20);
-
-    text(points.length / 2, windowWidth - 20, 30)
-    pop();
-
 }
 
 function draw(){
-    if(paused)
-        return;
+    background(220);
+    translate(0, windowHeight);
+    scale(1, -1);
 
-    background(0, 0,dark ? 0 : 86);
-    staticSetup();
+    translate(50, 50)
+
+    if(t >= duration)
+        timeStamp = millis();
 
     t = (millis() - timeStamp) / 1000;
+
+    displayAxes();
     env.display(sliderNumberLines.value());
-    
-    strokeWeight(3);
-    if(points.length > 1)
-        displayPoints()
+    displayVectors();
 
-    if (points.length > 2) {
-        if(t < duration && currentCurve === 0)
-            displayLines();
-
-        else if(t > duration && currentCurve === 0 && checkShowConstantSpeed.checked()) {
-            currentCurve++;
-            timeStamp = millis();
-        }
-
-        else if(t < duration && currentCurve === 1)
-            displayLinesSpeed()
-    }
-
-    displayCurves();
 }
 
-class Envelope{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.axisLength = 400
-    }
-    display(nLines){
-        if(nLines === 0)
-            return;
+function displayVectors(){
+    push();
+    strokeWeight(1);
+    let xa = x1 + t * (x0 - x1) / duration;
+    let ya = y1 + t * (y0 - y1) / duration;
 
-        console.log(nLines);
+    let xb = x0 + t * (x2 - x0) / duration;
+    let yb = y0 + t * (y2 - y0) / duration;
 
-        push();
-        stroke(dark ? 255 : 0);
-        strokeWeight(1);
+    let xc = xa + t * (xb - xa) / duration;
+    let yc = ya + t * (yb - ya) / duration;
 
-        line(this.x, this.y, this.x, this.y - this.axisLength);
-        line(this.x, this.y, this.x + this.axisLength, this.y);
+    line(0, 0, xa, ya);
+    createArrow(xa, ya, atan2(ya, xa));
 
-        let spacing = this.axisLength / (nLines + 1)
+    line(0, 0, xb, yb)
+    createArrow(xb, yb, atan2(yb, xb));
 
-        for(let i = 0; i <= nLines; i++)
-            line(this.x + this.axisLength - spacing * i, this.y, this.x, this.y - spacing * i);
+    line(xa, ya, xb, yb);
 
-        pop();
-    }
+    stroke(200, 0, 0)
+    strokeWeight(5);
+    point(xc, yc);
+    pop()
+
 }
 
-function displayLinesSpeed(){
-
-    let localPoints = new Float32Array(points);
-
-    for(let j = 0; j < (points.length) / 2 - 1; j++) {
-        let rx = localPoints[2 * j + 2 + x] - localPoints[2 * j + x];
-        let ry = localPoints[2 * j + 2 + y] - localPoints[2 * j + y];
-
-        let mag = sqrt(sq(rx) + sq(ry));
-
-        localPoints[2 * j + x] += constrain(400 * t * (rx / mag) / duration, -abs(rx), abs(rx));
-        localPoints[2 * j + y] += constrain(400 * t * (ry / mag) / duration, -abs(ry), abs(ry));
-    }
-
+function createArrow(x, y, a){
     push();
-    for(let i = 0; i < (points.length) / 2 - 2; i++){
-        checkRandomColor.checked() ? stroke(((i + 1) * rr) % 360, ((i + 1) * rg) % 50 + 50, ((i + 1) * rb) % 30 + 70) : stroke(dark ? 255 : 0);
-        for(let j = 0; j < (points.length) / 2 - i - 2; j++) {
 
-            line(localPoints[2 * j + x], localPoints[2 * j + y], localPoints[2 * j + 2 + x], localPoints[2 * j + 2 + y]);
+    translate(x - 2 * cos(a), y - 2 * sin(a));
+    rotate(a);
 
-            push();
-            stroke(dark ? 255 : 0);
-            strokeWeight(10);
-            point(localPoints[2 * j + x], localPoints[2 * j + y]);
-            pop();
+    beginShape();
+    noFill();
 
-            let rx = localPoints[2 * j + 2 + x] - localPoints[2 * j + x];
-            let ry = localPoints[2 * j + 2 + y] - localPoints[2 * j + y];
+    vertex(-10, -5);
+    vertex(0, 0);
+    vertex(-10, 5);
 
-            let mag = sqrt(sq(rx) + sq(ry));
-
-            localPoints[2 * j + x] += constrain(500 * t * (rx / mag) / duration, -abs(rx), abs(rx));
-            localPoints[2 * j + y] += constrain(500 * t * (ry / mag) / duration, -abs(ry), abs(ry));
-        }
-        push();
-        stroke(dark ? 255 : 0);
-        strokeWeight(10);
-        point(localPoints[points.length - 2 * i - 4 + x], localPoints[points.length - 2 * i - 4 + y]);
-        pop();
-    }
-
-    push();
-    stroke(205, 90, 90);
-    strokeWeight(10);
-    point(localPoints[x], localPoints[y]);
-    curvePointsSpeed.push(localPoints[x]);
-    curvePointsSpeed.push(localPoints[y]);
-    pop();
-
+    endShape();
     pop();
 }
 
-function displayLines(){
-
-    let localPoints = new Float32Array(points);
-
-    for(let j = 0; j < (points.length) / 2 - 1; j++) {
-        localPoints[2 * j + x] += t * (localPoints[2 * j + 2 + x] - localPoints[2 * j + x]) / duration;
-        localPoints[2 * j + y] += t * (localPoints[2 * j + 2 + y] - localPoints[2 * j + y]) / duration;
-    }
-
-    push();
-    for(let i = 0; i < (points.length) / 2 - 2; i++){
-        checkRandomColor.checked() ? stroke(((i + 1) * rr) % 360, ((i + 1) * rg) % 50 + 50, ((i + 1) * rb) % 30 + 70) : stroke(dark ? 255 : 0);
-        for(let j = 0; j < (points.length) / 2 - i - 2; j++) {
-
-            line(localPoints[2 * j + x], localPoints[2 * j + y], localPoints[2 * j + 2 + x], localPoints[2 * j + 2 + y]);
-
-            push();
-            stroke(dark ? 255 : 0);
-            strokeWeight(10);
-            point(localPoints[2 * j + x], localPoints[2 * j + y]);
-            pop();
-
-            localPoints[2 * j + x] += t * (localPoints[2 * j + 2 + x] - localPoints[2 * j + x]) / duration;
-            localPoints[2 * j + y] += t * (localPoints[2 * j + 2 + y] - localPoints[2 * j + y]) / duration;
-        }
-        push();
-        stroke(dark ? 255 : 0);
-        strokeWeight(10);
-        point(localPoints[points.length - 2 * i - 4 + x], localPoints[points.length - 2 * i - 4 + y]);
-        pop();
-    }
-
-    push();
-    stroke(0, 100, 80);
-    strokeWeight(10);
-    point(localPoints[x], localPoints[y]);
-    if(checkShowBezierCurve.checked()) {
-        curvePoints.push(localPoints[x]);
-        curvePoints.push(localPoints[y]);
-    }
-    pop();
-
-    pop();
-}
-
-function displayCurves(){
-    for(let i = 0; i < (curvePoints.length) / 2 - 1; i++){
-        strokeWeight(4);
-        stroke(0, 100, 80);
-        line(curvePoints[2 * i + x], curvePoints[2 * i + y], curvePoints[2 * i + 2 + x], curvePoints[2 * i + 2 + y]);
-    }
-
-    for(let i = 0; i < (curvePointsSpeed.length) / 2 - 1; i++){
-        strokeWeight(4);
-        stroke(205, 90, 90);
-        line(curvePointsSpeed[2 * i + x], curvePointsSpeed[2 * i + y], curvePointsSpeed[2 * i + 2 + x], curvePointsSpeed[2 * i + 2 + y]);
-    }
-}
-
-function displayPoints(){
-    push();
-    stroke(dark ? 255 : 0)
-    for(let i = 0; i < (points.length) / 2 - 1; i++)
-        line(points[2 * i + x], points[2 * i + y], points[2 * i + 2 + x], points[2 * i + 2 + y]);
-    pop();
+function displayAxes(){
+    line(0, 0, 0, 600);
+    line(0, 0, 600, 0);
 }
 
 function createUI(){
-    checkRandomColor = createCheckbox('Random colors', true);
-    checkRandomColor.position(12, 126);
-    checkRandomColor.style('font-family', 'Helvetica, serif');
-    checkRandomColor.style('color', dark ? '#FFFFFF': '#505050');
-    checkRandomColor.style('-webkit-user-select', 'none');
-    checkRandomColor.style('-ms-user-select', 'none');
-    checkRandomColor.style('user-select', 'none');
-
-    checkShowBezierCurve = createCheckbox('Show bezier curve', true);
-    checkShowBezierCurve.position(12, 170);
-    checkShowBezierCurve.style('font-family', 'Helvetica, serif');
-    checkShowBezierCurve.style('color', dark ? '#FFFFFF': '#505050');
-    checkShowBezierCurve.style('-webkit-user-select', 'none');
-    checkShowBezierCurve.style('-ms-user-select', 'none');
-    checkShowBezierCurve.style('user-select', 'none');
-
-    checkShowConstantSpeed = createCheckbox('Show constant speed curve', true);
-    checkShowConstantSpeed.position(12, 192);
-    checkShowConstantSpeed.style('font-family', 'Helvetica, serif');
-    checkShowConstantSpeed.style('color', dark ? '#FFFFFF': '#505050');
-    checkShowConstantSpeed.style('-webkit-user-select', 'none');
-    checkShowConstantSpeed.style('-ms-user-select', 'none');
-    checkShowConstantSpeed.style('user-select', 'none');
-
-    checkBackgroundColor = createCheckbox('Dark background', dark);
-    checkBackgroundColor.position(12, 148);
-    checkBackgroundColor.style('font-family', 'Helvetica, serif');
-    checkBackgroundColor.style('color', dark ? '#FFFFFF': '#505050');
-    checkBackgroundColor.style('-webkit-user-select', 'none');
-    checkBackgroundColor.style('-ms-user-select', 'none');
-    checkBackgroundColor.style('user-select', 'none');
-    checkBackgroundColor.changed(() => {
-        dark = !dark;
-        checkBackgroundColor.style('color', dark ? '#FFFFFF': '#505050');
-        checkRandomColor.style('color', dark ? '#FFFFFF': '#505050');
-        checkShowBezierCurve.style('color', dark ? '#FFFFFF' : '#505050');
-        checkShowConstantSpeed.style('color', dark ? '#FFFFFF' : '#505050');
-    });
-
     sliderNumberLines = createSlider(0, 100, 0, 1);
-    sliderNumberLines.position(15, windowHeight - 35);
+    sliderNumberLines.position(15, 35);
     sliderNumberLines.style('width', 100);
-
 }
 
-function keyPressed(){
-    if(keyCode === 32) {            //SPACE_KEY
-        if(!paused) {
-            timeStamp = millis();
-            rr = random(360);
-            rg = random(50);
-            rb = random(30);
-            curvePoints = []
-            curvePointsSpeed = [];
-            currentCurve = 0;
-        }else {
-            paused = false;
-            timeStamp = millis() - pausedT * 1000
-        }
+class Envelope{
+
+    display(nLines){
+
+        push();
+        stroke(0);
+        strokeWeight(1);
+
+        line(x0, y0, x1, y1);
+        line(x0, y0, x2, y2);
+
+        if(nLines === 0)
+            return;
+
+        let spacing = axesLength / (nLines + 1);
+
+        for(let i = 0; i <= nLines; i++)
+            line(x2 - spacing * i, y0, x0, y0 + spacing * i);
+
+        pop();
     }
-
-    else if(keyCode === 67)         // C key
-        clearScreen()
-
-    else if(keyCode === 80) {        // P Key
-        paused = !paused;
-
-        if(paused)
-            pausedT = t;
-
-        else
-            timeStamp = millis() - pausedT * 1000
-    }
-}
-
-function clearScreen(){
-    points = new Float32Array(0);
-    curvePoints = [];
-    curvePointsSpeed = [];
-    timeStamp = -(duration * 1000 + 1);
-    currentCurve = -1;
-}
-
-function touchStarted(){
-    if(!(mouseX < 250 && mouseY < 300)) {
-        let newPoints = new Float32Array(points.length + 2);
-        newPoints.set(points);
-        newPoints.set([mouseX, mouseY], points.length);
-
-        points = newPoints;
-    }
-}
-
-function textArray(arr, column){
-    textSize(14);
-    textAlign(LEFT);
-
-    for(let i = 0; i < arr.length; i++)
-        text("[" + arr[i] + "]", column * 300 + 15,20 * (i + 1));
 }
